959efbc7b8258be7e77dea24dd73bbbf4483a04a
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-07-26 15:05:35 -0300

    Mapeamento completo da PageMyAccount finalizado

--- src/test/java/pagesModel/PageMyAccount.java
+++ src/test/java/pagesModel/PageMyAccount.java
@@ -1,6 +1,17 @@
 package pagesModel;
 
+import org.openqa.selenium.By;
+
 public class PageMyAccount {
 
     private final String linkMyAccount = "http://automationpractice.com/index.php?controller=my-account";
+    private By buttonSignOut = By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[2]/a");
+
+    public String getLinkMyAccount() {
+        return linkMyAccount;
+    }
+
+    public By getButtonSignOut() {
+        return buttonSignOut;
+    }
 }
