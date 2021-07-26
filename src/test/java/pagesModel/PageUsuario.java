6fe5cb28ea00cc76f3cef92e81b0912a84c4a65e
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-07-26 15:03:14 -0300

    Mapeamento completo da PageUsuario finalizado

---
+++ src/test/java/pagesModel/PageUsuario.java
@@ -0,0 +1,194 @@
+package pagesModel;
+
+public class PageUsuario {
+
+    private String userFirstName;
+    private String userLastName;
+    private String password;
+    private String email;
+    private int diaNascimento;
+    private int mesNascimento;
+    private int anoNascimento;
+    private boolean newLetter;
+    private boolean specialOfter;
+    private String firstName2;
+    private String lastName2;
+    private String company;
+    private String address;
+    private String addressLine2;
+    private String city;
+    private int state;
+    private String postalCode;
+    private int country;
+    private String additionalInformation;
+    private String homePhone;
+    private String mobilePhone;
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDiaNascimento() {
+        return diaNascimento;
+    }
+
+    public void setDiaNascimento(int diaNascimento) {
+        this.diaNascimento = diaNascimento;
+    }
+
+    public int getMesNascimento() {
+        return mesNascimento;
+    }
+
+    public void setMesNascimento(int mesNascimento) {
+        this.mesNascimento = mesNascimento;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public boolean isNewLetter() {
+        return newLetter;
+    }
+
+    public void setNewLetter(boolean newLetter) {
+        this.newLetter = newLetter;
+    }
+
+    public boolean isSpecialOfter() {
+        return specialOfter;
+    }
+
+    public void setSpecialOfter(boolean specialOfter) {
+        this.specialOfter = specialOfter;
+    }
+
+    public String getFirstName2() {
+        return firstName2;
+    }
+
+    public void setFirstName2(String firstName2) {
+        this.firstName2 = firstName2;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getCountry() {
+        return country;
+    }
+
+    public void setCountry(int country) {
+        this.country = country;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+}
