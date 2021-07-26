a0e4f92e30650e74a29baf7c12941d788067cf04
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-07-26 14:05:06 -0300

    Mapeamento da PageIndex

--- src/test/java/pagesModel/PageIndex.java
+++ src/test/java/pagesModel/PageIndex.java
@@ -4,17 +4,17 @@ import org.openqa.selenium.By;
 
 public class PageIndex {
 
-    private String user = "";
-    private String password = "";
-    private String email = "alessandrosilva.2705@aluno.saojudas.br";
-
     //Links utilizados na pagina
     private String linkIndex = "http://automationpractice.com/index.php";
-    private String linkSignIn = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
 
     //Endereços de componentes da pagina index
     private By buttonSignin = By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a");
-    private By caixaTextoEmail = By.xpath("//*[@id=\"email_create\"]");
-    private By createAnAccount = By.xpath("//*[@id=\"SubmitCreate\"]");
-    private By
+
+    public By getButtonSignin() {
+        return buttonSignin;
+    }
+
+    public String getLinkIndex() {
+        return linkIndex;
+    }
 }
