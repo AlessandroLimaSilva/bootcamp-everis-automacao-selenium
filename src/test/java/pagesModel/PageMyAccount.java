ff414dedcdfccfda295cdc283fa27fe4d29340aa
AlessandroLimaDaSilva <coderlima@gmail.com>
2021-07-26 22:48:49 -0300

    Adicionado a parte logica do teste criação de usuario

--- src/test/java/pagesModel/PageMyAccount.java
+++ src/test/java/pagesModel/PageMyAccount.java
@@ -1,11 +1,16 @@
 package pagesModel;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class PageMyAccount {
 
+    private WebDriver driver;
     private final String linkMyAccount = "http://automationpractice.com/index.php?controller=my-account";
     private By buttonSignOut = By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[2]/a");
+
+
+    public PageMyAccount(WebDriver driver) { this.driver = driver; }
 
     public String getLinkMyAccount() {
         return linkMyAccount;
